c23a2661c1bb9618319f04823076786e8d65e5c0
lithiumagic <mgcusr@gmail.com>
2026-07-10 15:37:53 -0700

    REPL

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin) // when it runs it blocks and waits for input until user presses Enter
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		userInput = strings.ToLower(userInput)
+		cleanUserInput := strings.Fields(userInput)
+		if len(cleanUserInput) == 0 {
+			continue
+		}
+		firstWord := cleanUserInput[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 
 }
