8a3cac862a991666e39e1f29cf45a4d1ae7d6424
lithiumagic <mgcusr@gmail.com>
2026-07-10 21:34:39 -0700

    help and exit commands

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin) // when it runs it blocks and waits for input until user presses Enter
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		userInput := scanner.Text()
-		userInput = strings.ToLower(userInput)
-		cleanUserInput := strings.Fields(userInput)
-		if len(cleanUserInput) == 0 {
-			continue
-		}
-		firstWord := cleanUserInput[0]
-		fmt.Printf("Your command was: %s\n", firstWord)
-	}
-
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,13 +1,62 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func cleanInput(text string) []string {
-	splitStringSlice := []string{}
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin) // when it runs it blocks and waits for input until user presses Enter
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
 
-	text = strings.ToLower(text)
-	splitStringSlice = strings.Fields(text)
-	return splitStringSlice
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+		commandName := words[0]
+
+		cmd, exists := getCommands()[commandName]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		} else {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		}
+	}
+
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
